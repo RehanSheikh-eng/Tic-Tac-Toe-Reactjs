e1dabf765701aead2e7d84a9a5ac690cd6f4e821
Rehan Sheikh <rs2126@cam.ac.uk>
2021-08-04 16:55:34 +0100

    added 3 components

---
+++ src/components/game/index.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+
+class Game extends Component{
+    render(){ 
+        return (
+            <div className="game">
+              <div className="game-board">
+                <Board />
+              </div>
+              <div className="game-info">
+                <div>{/* status */}</div>
+                <ol>{/* TODO */}</ol>
+              </div>
+            </div>
+        );
+    }
+}
---
+++ src/components/square/index.js
@@ -0,0 +1,12 @@
+import {React, Component} from 'react';
+
+class Square extends Component{
+    render(){
+        <button className="square">
+            {this.props.value}
+        </button>
+    }
+
+}
+
+export default Square;
