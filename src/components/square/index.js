903b076715681b910c82ce22c961bf9740a4e327
Rehan Sheikh <rs2126@cam.ac.uk>
2021-08-04 17:51:25 +0100

    handleClick function and Square is now functional component

--- src/components/square/index.js
+++ src/components/square/index.js
@@ -2,27 +2,12 @@ import { getAllByAltText } from '@testing-library/react';
 import {React, Component} from 'react';
 import '../../index.css'
 
-class Square extends Component{
-    constructor(props){
-
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
-    render(){
-        return(
-            <button 
-            className="square"
-            onClick={() => this.setState({value: "X"})}>
-
-                {this.state.value}
-                
-            </button>
-        );
-    }
-
-}
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
 
 export default Square;
